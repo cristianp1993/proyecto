6cfe0662e22b48e4ba1aaa9074848dba2d2a670b
unknown <cristianpie@misena.edu.co>
2019-05-23 22:12:25 -0500

    'recuperado'

--- miProyecto-angular/src/app/app.module.ts
+++ miProyecto-angular/src/app/app.module.ts
@@ -1,33 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 //el routeo de componentes creados
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { LoginComponent } from './componentes/login/login.component';
-import { ShareComponent } from './componentes/share/share.component';
 import { SignupComponent } from './componentes/signup/signup.component';
 import { ProfileComponent } from './componentes/profile/profile.component';
 import { PasswordComponent } from './componentes/password/password.component';
 import { RequestResetComponent } from './componentes/request-reset/request-reset.component';
 import { ResponseResetComponent } from './componentes/response-reset/response-reset.component';
 
+
+// libreria para usar el sidebar
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 //importacion del formModulo
 import { FormsModule } from "@angular/forms";
-
 //importamos libreria http para hacer peticiones a servicios
 import { HttpClientModule } from "@angular/common/http";
 
 // Libreria de notificacion
 import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    ShareComponent,
     SignupComponent,
     ProfileComponent,
     PasswordComponent,
@@ -39,7 +41,13 @@ import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    SnotifyModule
+    SnotifyModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     { provide: 'SnotifyToastConfig', useValue: ToastDefaults},
